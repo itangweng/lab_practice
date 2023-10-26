83a8a7b7443bbd533a66cd835db7bf2f831ba833
I-Tang, Weng <yitangweng@gmail.com>
2023-10-26 13:13:27 +0800

    computer program HW5 - Caear code

--- caesarcode/caesarcode.cpp
+++ caesarcode/caesarcode.cpp
@@ -4,10 +4,23 @@
 using namespace std;
 
 int main(){
-    
-
-
-
+    int a;
+    char lec;
+    cin >> a;
+    a %=26;
+    cin.get();
+    while(cin.get(lec)){
+        if (lec=='\n')
+            break;
+        else if ((unsigned int)lec > 96 && (unsigned int)lec <123){
+            lec = (char)((unsigned int) lec + a );
+            if ((unsigned int) lec < 97)
+                lec = (char) ((unsigned int) lec + 26);
+            else if ((unsigned int) lec > 122)
+                lec = (char) ((unsigned int) lec - 26);
+        }
+        cout << lec;
+    }
 
     return 0;
 }
